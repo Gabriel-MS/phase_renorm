Fixed derivative properties errors

src/derivativeprop.py
@@ -2,6 +2,7 @@ import numpy as np
 import menus
 import correlations
 import data
+import eos
 
 import matplotlib
 matplotlib.use('TkAgg')
@@ -44,7 +45,7 @@ def calc_isothermal_dev_prop_pure(T,f,P,rho,h,IDs):
     
     #Derivatives
     d2AdT2 = (A2 - 2*A1 + A0)/(h**2)
-    d2FdT2 = (F2 - 2*F1 + A0)/(h**2)
+    d2FdT2 = (F2 - 2*F1 + F0)/(h**2)
     
     dAdT = (A2 - A0)/(2*h)
     dFdT = (F2 - F0)/(2*h)
@@ -94,6 +95,7 @@ def calc_isothermal_dev_prop_pure(T,f,P,rho,h,IDs):
     Cv = -T1*d2AdT2
     #Cv = -R*T1*T1*d2FdT2-2*R*T1*dFdT
     Cv = Cv[0]
+    print T1
     
     #Isothermal compression coefficient
     inv_kT = rhov*dPdrho
@@ -116,11 +118,14 @@ def calc_isothermal_dev_prop_pure(T,f,P,rho,h,IDs):
     #Speed of Sound
     Mw = data.mass(IDs)[0]
     Cp_ig = correlations.ideal_cp(IDs,T1[0])
-    Cp1 = Cp*R + Cp_ig
+    Cp1 = Cp + Cp_ig
     Cv_ig = Cp_ig - R
-    Cv1 = Cv*R + Cv_ig
+    Cv1 = Cv + Cv_ig
     w = (Cp1/Cv1*dPdrho/Mw*1e6)**(0.5)
     #w = (-(Vv**2)*Cp/Cv*dPdV/Mw)**(0.5)
+    
+    #for i in range(0,len(f1[0])):
+    #    print rho1[0][i],f1[0][i],'wow'
     
     deriv_data = []
     deriv_data.append(Pv)
@@ -129,16 +134,84 @@ def calc_isothermal_dev_prop_pure(T,f,P,rho,h,IDs):
     deriv_data.append(dAdT[0])
     deriv_data.append(Cv)
     deriv_data.append(dPdrho)
-    deriv_data.append(inv_kT)
-    deriv_data.append(kT)
-    deriv_data.append(lnkT)
+    deriv_data.append(inv_kT) #inv_kT
+    deriv_data.append(kT) #kT
+    deriv_data.append(lnkT) #lnkT
     deriv_data.append(dPdT[0])
     deriv_data.append(alfa)
     deriv_data.append(uJT)
     deriv_data.append(Cp)
-    deriv_data.append(w)
-    deriv_data.append(Cp1)
-    deriv_data.append(Cv1)
+    deriv_data.append(w) #w
+    deriv_data.append(Cp1) #Cp1
+    deriv_data.append(Cv1) #Cv1
+    
+    return deriv_data
+#==================================================================================================
+
+#Function to calculate derivative properties of pure compounds after renormalization--------------- 
+def calc_isothermal_dev_prop_pure_analytical(T,A,P,V,IDs,EoS,MR,kij):
+    
+    b = eos.b_calc(IDs,EoS)
+    x = np.array([0.999,0.001])
+    bmix = eos.bmix_calc(MR,b,x)
+    a = eos.a_calc(IDs,EoS,T)
+    amix = eos.amix_calc(MR,a,x,kij)
+    ac = eos.ac_calc(IDs,EoS,T)[0]
+    m = eos.kapa_calc(IDs,EoS)[0]
+    Tc = np.array(data.Tc(IDs))[0]
+    rho = 1/V
+    
+    d2adT2 = ac*m*(1+m)*np.sqrt(Tc/T)/(2*T*Tc)
+    B = bmix*P/(R*T)
+    Z = P*V/(R*T)
+    
+    f = A/V
+    
+    noll = np.ones((len(V)))
+    
+    Cv_res = (T*d2adT2/(bmix*np.sqrt(8)))*np.log((Z+B*(1+np.sqrt(2)))/(Z+B*(1-np.sqrt(2))))
+    
+    d2AdT2 = -d2adT2/bmix/np.sqrt(8)*np.log((1+rho*bmix*(1+np.sqrt(2)))/(1+rho*bmix*(1-np.sqrt(2))))
+    
+    h = 5e-2
+    T_p = T+T*h
+    T_m = T-T*h
+    
+    a_p = eos.a_calc(IDs,EoS,T_p)
+    amix_p = eos.amix_calc(MR,a_p,x,kij)
+
+    a_m = eos.a_calc(IDs,EoS,T_m)
+    amix_m = eos.amix_calc(MR,a_m,x,kij)
+    
+    A = R*T*np.log(rho/(1-rho*bmix))-amix/bmix/np.sqrt(8)*np.log((1+rho*bmix*(1+np.sqrt(2)))/(1+rho*bmix*(1-np.sqrt(2))))
+    A_p = R*T_p*np.log(rho/(1-rho*bmix))-amix_p/bmix/np.sqrt(8)*np.log((1+rho*bmix*(1+np.sqrt(2)))/(1+rho*bmix*(1-np.sqrt(2))))
+    A_m = R*T_m*np.log(rho/(1-rho*bmix))-amix_m/bmix/np.sqrt(8)*np.log((1+rho*bmix*(1+np.sqrt(2)))/(1+rho*bmix*(1-np.sqrt(2))))
+        
+    d2AdT2_2 = (A_p-2*A+A_m)/((T*h)**2)
+    
+    A = A*1e8
+    A_p = A_p*1e8
+    A_m = A_m*1e8
+    d2AdT2 = d2AdT2*1e6
+    d2AdT2_2 = d2AdT2_2*1e6
+    
+    deriv_data = []
+    deriv_data.append(P)
+    deriv_data.append(rho)
+    deriv_data.append(A)
+    deriv_data.append(A_p)
+    deriv_data.append(Cv_res)
+    deriv_data.append(A_m)
+    deriv_data.append(d2AdT2_2) 
+    deriv_data.append(d2AdT2)
+    deriv_data.append(noll)
+    deriv_data.append(noll)
+    deriv_data.append(noll)
+    deriv_data.append(noll)
+    deriv_data.append(noll)
+    deriv_data.append(noll)
+    deriv_data.append(noll)
+    deriv_data.append(noll)
     
     return deriv_data
 #==================================================================================================

src/envelope.py
@@ -651,7 +651,7 @@ def Pxy_envelope(T,IDs,EoS,MR,kij,nc,AR,CR,SM,r_data):
     
     #Definitions--------------------------------------------------
     #Main iteration conditions
-    x = np.array([0.01,0.99]) #x array
+    x = np.array([0.001,0.999]) #x array
     xf = 0.995                    #Main stop condition
     stepx = 5e-3                #Main step
     it = 0                      #Iteration counter
@@ -680,7 +680,8 @@ def Pxy_envelope(T,IDs,EoS,MR,kij,nc,AR,CR,SM,r_data):
     print Psat,x,P,y
     Vv = R*T/P #Not going to be used, just starting
     Vl = 0.99  #Not going to be used, just starting
-    P = P/10
+    P = P/100
+    print 'P=',P
     #=============================================================
     
     #Main iteration start, range x1------------------------------------------
@@ -1549,7 +1550,7 @@ def PV_findTc2_envelope(EoS,IDs,MR,T,Tfinal,stepT,nd,nx,kij,nc,CR,en_auto,beta_a
         Fobj = ans[5]
         flagI = ans[6]
 
-        #print T,ans[0],ans[1],ans[2],ans[5],ans[6],i
+        print T,ans[0],ans[1],ans[2],ans[5],ans[6],i
 
         if Fobj>1e-5:
             step = -1e0
@@ -1754,6 +1755,10 @@ def PV_deriv_calc_envelope(EoS,IDs,MR,T,Tfinal,stepT,nd,nx,kij,nc,CR,en_auto,bet
     fres1 = []
     fres2 = []
     
+    f0 = []
+    f1 = []
+    f2 = []
+    
     P0 = []
     P1 = []
     P2 = []
@@ -1766,60 +1771,97 @@ def PV_deriv_calc_envelope(EoS,IDs,MR,T,Tfinal,stepT,nd,nx,kij,nc,CR,en_auto,bet
     T1 = []
     T2 = []
     
-    h = 1e-2
+    h = 5e-2
 
     step = 1.0
     finalT = T
     
-    print 'T:   dens_vap:   dens_liq:   P:'
-    while T<=finalT:
+    if EoS==2 or EoS==4 or EoS==6:
+        #print 'T:   dens_vap:   dens_liq:   P:'
+        while T<=finalT:
 
-        T0.append(T-T*h)
-        T1.append(T)
-        T2.append(T+T*h)
+            T0.append(T-T*h)
+            T1.append(T)
+            T2.append(T+T*h)
         
-        ren = renormalization.renorm(EoS,IDs,MR,T,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
-        fres1.append(ren[7])
-        rho1.append(ren[2])
-        P1.append(ren[8])
-        #print 'central',T,dens[0],dens[1],dens[2],Fobj
+            ren = renormalization.renorm(EoS,IDs,MR,T,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
+            fres1.append(ren[7])
+            rho1.append(ren[2])
+            P1.append(ren[8])
+            f1.append(ren[0])
+            #print 'central',T,dens[0],dens[1],dens[2],Fobj
 
-        ren = renormalization.renorm(EoS,IDs,MR,T+T*h,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
-        fres2.append(ren[7])
-        rho2.append(ren[2])
-        P2.append(ren[8])
-        #print 'plus',T,dens[0],dens[1],dens[2],Fobj_plus
+            ren = renormalization.renorm(EoS,IDs,MR,T+T*h,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
+            fres2.append(ren[7])
+            rho2.append(ren[2])
+            P2.append(ren[8])
+            f2.append(ren[0])
+            #print 'plus',T,dens[0],dens[1],dens[2],Fobj_plus
 
-        ren = renormalization.renorm(EoS,IDs,MR,T-T*h,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
-        fres0.append(ren[7])
-        rho0.append(ren[2])
-        P0.append(ren[8])
-        #print 'minus',T,dens[0],dens[1],dens[2],Fobj_minus
+            ren = renormalization.renorm(EoS,IDs,MR,T-T*h,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
+            fres0.append(ren[7])
+            rho0.append(ren[2])
+            P0.append(ren[8])
+            f0.append(ren[0])
+            #print 'minus',T,dens[0],dens[1],dens[2],Fobj_minus
 
-        T = T+step
+            T = T+step
     
-    T_list = []
-    fres_list = []
-    P_list = []
-    rho_list = []
+        T_list = []
+        fres_list = []
+        P_list = []
+        rho_list = []
+        f_list = []
     
-    T_list.append(T0)
-    T_list.append(T1)
-    T_list.append(T2)
+        T_list.append(T0)
+        T_list.append(T1)
+        T_list.append(T2)
     
-    fres_list.append(fres0)
-    fres_list.append(fres1)
-    fres_list.append(fres2)
+        fres_list.append(fres0)
+        fres_list.append(fres1)
+        fres_list.append(fres2)
+        
+        f_list.append(f0)
+        f_list.append(f1)
+        f_list.append(f2)
     
-    P_list.append(P0)
-    P_list.append(P1)
-    P_list.append(P2)
+        P_list.append(P0)
+        P_list.append(P1)
+        P_list.append(P2)
     
-    rho_list.append(rho0)
-    rho_list.append(rho1)
-    rho_list.append(rho2)
+        rho_list.append(rho0)
+        rho_list.append(rho1)
+        rho_list.append(rho2)
     
-    der_prop = derivativeprop.calc_isothermal_dev_prop_pure(T_list,fres_list,P_list,rho_list,h,IDs)
+        der_prop = derivativeprop.calc_isothermal_dev_prop_pure(T_list,f_list,P_list,rho_list,T*h,IDs)
+        
+    else:
+        n = 500
+        b = eos.b_calc(IDs,EoS)
+        x = np.array([0.999,0.001])
+        bmix = eos.bmix_calc(MR,b,x)
+        a = eos.a_calc(IDs,EoS,T)
+        amix = eos.amix_calc(MR,a,x,kij)
+        V = np.empty((n))
+        rho = np.empty((n))
+        for i in range(0,n):
+            rho[i] = np.array(float(i)/n/bmix)
+        rho[0] = 1e-8
+        V = 1/rho
+        f = rho*R*T*np.log(rho/(1-rho*bmix))-rho*amix/bmix/np.sqrt(8)*np.log((1+rho*bmix*(1+np.sqrt(2)))/(1+rho*bmix*(1-np.sqrt(2))))
+        A = f*V
+        
+        print T,R,bmix,amix
+        
+        #rho = np.array(menus.flatten(rho))
+        #f = np.array(menus.flatten(f))
+        
+        fspl = splrep(rho,f,k=3)
+        u = splev(rho,fspl,der=1)
+        
+        P = -f+rho*u
+        
+        der_prop = derivativeprop.calc_isothermal_dev_prop_pure_analytical(T,A,P,V,IDs,EoS,MR,kij)
     
     return der_prop
 #====================================================================================== 
@@ -1934,7 +1976,7 @@ def calc_env(user_options,print_options,nc,IDs,EoS,MR,z,AR,CR,P,T,kij,auto,en_au
 
     if env_type==5:
         #Calculate pure PV derivative properties*****************************************************
-        nd = 400
+        nd = 500
         nx = 200
         n = 5
         stepT = 0.5
@@ -1964,16 +2006,16 @@ def calc_env(user_options,print_options,nc,IDs,EoS,MR,z,AR,CR,P,T,kij,auto,en_au
         nd = 400
         nx = 200
         n = 8
-        finalT = 700.0
-        stepT = 2.5
+        finalT = 520.0
+        stepT = 5.0
         print '\nCalculating PV envelope'
         if env_type==2:
             env_PV = PV_envelope(EoS,IDs,MR,T,finalT,stepT,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n)
         if env_type==3:
             env_PV = PV_estimate_Tc_envelope(EoS,IDs,MR,T,finalT,stepT,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n)
         if env_type==4:
-            #env_PV = PV_findTc3_envelope(EoS,IDs,MR,T,finalT,stepT,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,False,0,0)
-            env_PV = PV_findTc2_envelope(EoS,IDs,MR,T,finalT,stepT,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
+            env_PV = PV_findTc3_envelope(EoS,IDs,MR,T,finalT,stepT,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,False,0,0)
+            #env_PV = PV_findTc2_envelope(EoS,IDs,MR,T,finalT,stepT,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
         print 'PV envelope calculated'
 
         print 'Creating pure PV report'
@@ -2025,12 +2067,17 @@ def calc_env(user_options,print_options,nc,IDs,EoS,MR,z,AR,CR,P,T,kij,auto,en_au
             nd = 40
             nx = 40
             n = 5
-            r_data = renormalization.renorm(EoS,IDs,MR,T,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
+            #r_data = renormalization.renorm(EoS,IDs,MR,T,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
             print '\nHelmholtz Energy Surface calculated and reported'
         else:
+            nd = 40
+            nx = 40
+            n = 5
             r_data = []
-
-        critical.sadus_hicks_young(r_data[4],r_data[3],r_data[1],r_data[9])
+        
+        x = np.array([0.1,0.9])
+        renormalization.crit_mix(False,0,0,EoS,IDs,MR,T,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,x)
+        #critical.sadus_hicks_young(r_data[4],r_data[3],r_data[1],r_data[9])
         #*******************************************************************************
 
     if env_type==8 or env_type==9 or env_type==10:

src/eos.py
@@ -139,15 +139,40 @@ def alfa_calc(IDs,EoS,T):
             kapa = 0.48508+1.55171*omega-0.15613*omega*omega
     elif EoS==3 or EoS==4: #PR, PR+RG
         if omega[0]<0.2:
-            kapa = 0.37640+1.54230*omega-0.26990*omega*omega
+            #kapa = 0.37640+1.54230*omega-0.26990*omega*omega
+            kapa = 0.37464+1.54226*omega-0.26992*omega*omega
         else:
-            kapa = 0.3796+(1.4850+(-0.1644+0.01667*omega)*omega)*omega
+            kapa = 0.37464+1.54226*omega-0.26992*omega*omega
+            #kapa = 0.3796+(1.4850+(-0.1644+0.01667*omega)*omega)*omega
     else: #CPA, CPA+RG
         kapa = np.array(data.c1(IDs))
         
     alfa = (1.0+kapa*(1.0-(Tr**0.5)))**2
     
     return alfa
+#=============================================================
+
+#Calculates kapa parameter------------------------------------
+def kapa_calc(IDs,EoS):
+    omega = np.array(data.omega(IDs))
+    
+    #Calculate kapa
+    if EoS==1 or EoS==2: #SRK, SRK+RG
+        if omega[0]<0.2:
+            kapa = 0.48508+1.55171*omega-0.15613*omega*omega
+        else:
+            kapa = 0.48508+1.55171*omega-0.15613*omega*omega
+    elif EoS==3 or EoS==4: #PR, PR+RG
+        if omega[0]<0.2:
+            #kapa = 0.37640+1.54230*omega-0.26990*omega*omega
+            kapa = 0.37464+1.54226*omega-0.26992*omega*omega
+        else:
+            kapa = 0.37464+1.54226*omega-0.26992*omega*omega
+            #kapa = 0.3796+(1.4850+(-0.1644+0.01667*omega)*omega)*omega
+    else: #CPA, CPA+RG
+        kapa = np.array(data.c1(IDs))
+    
+    return kapa
 #=============================================================
 
 #Cubic, calculates alfa prime parameter-----------------------

src/estimation.py
@@ -297,7 +297,7 @@ def objFunc_dens_Psat_Tc(par,argss):
     Fobj_Tc = abs((Tc_calc-Tc_exp)/Tc_exp)
     Fobj_Pc = abs((Pc_calc-Pc_exp)/Pc_exp)
     Fobj_rhoc    = abs((rhoc_calc-rhoc_exp)/rhoc_exp)
-    Fobj      = Fobj_Tc + Fobj_Pc + Fobj_rhoc
+    Fobj      = 10*Fobj_Tc + 10*Fobj_Pc + 10*Fobj_rhoc
     
     print '--------------------------------'
     print 'Parameters:',L__est,phi__est
@@ -633,10 +633,10 @@ def Estimate_Parameters_crit(EoS,IDs,MR,T,Tfinal,stepT,nd,nx,kij,nc,CR,en_auto,b
     #Create boundaries
     bmax = np.empty((2))
     bmin = np.empty((2))
-    bmin[0] = 1E-10
-    bmax[0] = 10E-10
-    bmin[1] = 0.10
-    bmax[1] = 10.00
+    bmin[0] = 5.20E-10
+    bmax[0] = 5.90E-10
+    bmin[1] = 1.00
+    bmax[1] = 2.30
     
     #Organize Parameters
     #Organize Parameters
@@ -668,23 +668,23 @@ def Estimate_Parameters_crit(EoS,IDs,MR,T,Tfinal,stepT,nd,nx,kij,nc,CR,en_auto,b
             p[i][j] = np.random.uniform(bmin[j],bmax[j])
             #p[i][1] = 1.0
     
-    #p[0][0] = 7.207e-10
-    #p[0][1] = 0.60
+    p[0][0] = 5.5e-10
+    p[0][1] = 1.70
     
     print 'particles'
     print p
     
     #Initialize PSO method - ALL PARAMETERS
-    best = PSO.PSO(nparameter,ndata,nswarm,objFunc_dens_Psat_crit,argss,p,bmin,bmax)
-    best_param = best[0]
-    param_list = best[1]
-    param_Fobj = best[2]
-    
-    #Initialize PSO method to fit parameter 1 (L)
-    #best = PSO.PSO(nparameter,ndata,nswarm,objFunc_dens_Psat_Tc,argss,p,bmin,bmax)
+    #best = PSO.PSO(nparameter,ndata,nswarm,objFunc_dens_Psat_crit,argss,p,bmin,bmax)
     #best_param = best[0]
     #param_list = best[1]
     #param_Fobj = best[2]
+    
+    #Initialize PSO method to fit parameter 1 (L)
+    best = PSO.PSO(nparameter,ndata,nswarm,objFunc_dens_Psat_Tc,argss,p,bmin,bmax)
+    best_param = best[0]
+    param_list = best[1]
+    param_Fobj = best[2]
 
     #Initialize PSO method to fit parameter 2 (phi)
     #best = PSO.PSO(nparameter,ndata,nswarm,objFunc_dens_Psat_crit,argss,p,bmin,bmax)

src/main.py
@@ -18,9 +18,9 @@ print ('    ============================================\n\n\n')
 #User menu to define custom configuration
 #user_options = menus.user()
 user_options = []
-user_options.append(2) #nc
-user_options.append([12,17]) #IDs
-user_options.append(6) #EoS
+user_options.append(1) #nc
+user_options.append([1,1]) #IDs
+user_options.append(3) #EoS
 user_options.append(1) #MR
 user_options.append([0.3,0.7]) #z
 user_options.append([3,3]) #CPA AM
@@ -49,9 +49,9 @@ MR = user_options[3] #Define Mixing Rule
 z = user_options[4] #Define Feed Composition
 AR = user_options[5] #Define association rules - CPA
 CR = user_options[6] #Define combining rule - CPA
-env_type = 6
+env_type = 5
 P = 0.05
-T = 311.00
+T = 300.0
 kij = np.zeros((nc,nc))
 
 #CPA auto-association configurations

src/renormalization.py
@@ -113,9 +113,9 @@ def renorm(EoS,IDs,MR,T,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,estimate,L_est,ph
             k = k+1
             
         f_orig = f                                #Initial helmholtz energy density
+    
         #Subtract attractive forces (due long range correlations)
         f = f + 0.5*amix*(rho**2)
-
 
         #Adimensionalization
         rho = rho*bmix
@@ -158,7 +158,8 @@ def renorm(EoS,IDs,MR,T,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,estimate,L_est,ph
         f = f - 0.5*amix*(rho**2)
         
         #Store residual value of f
-        fres = f - rho*R*T*(np.log(rho)-1)
+        #fres = f - rho*R*T*(np.log(rho)-1) #WRONG
+        fres = f - rho*R*T*np.log(rho)
         #f = f + rho*R*T*(np.log(rho)-1) #Already accounting ideal gas energy
 
         #if(EoS==6):
@@ -179,6 +180,10 @@ def renorm(EoS,IDs,MR,T,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,estimate,L_est,ph
             u[i] = (f[i+1]-f[i-1])/(2*drho)
         u[nd-1] = (f[nd-1]-f[nd-2])/drho
         u[0] = (f[1]-f[0])/drho
+        
+        fspl = splrep(rho,f,k=3)         #Cubic Spline Representation
+        f = splev(rho,fspl,der=0)
+        u = splev(rho,fspl,der=1)        #Evaluate Cubic Spline First derivative
 
         P = -f+rho*u
         Pv.append(P)
@@ -428,6 +433,12 @@ def volume_renorm(phase, xint, Pint, bmix, R, T, r_data):
         #print rho
         #print Pvec
         #print Pfvec
+        
+        #NEW
+        P_fvec_spl = splrep(rho,Pfvec,k=3)         #Cubic Spline Representation
+        Pfvec = splev(rho,P_fvec_spl,der=0)
+        #NEW
+        
         #plt.plot(rho,Pvec)
         #plt.ylim(-15,15)
         #plt.show()
@@ -435,7 +446,7 @@ def volume_renorm(phase, xint, Pint, bmix, R, T, r_data):
         min1 = int(0.90*nd) #it was 0.90 before
         max2 = max1+2
         min2 = min1-2
-        #print rho[max1],rho[max2],max1,max2,min1,min2
+        print rho[max1],rho[max2],max1,max2,min1,min2
         #raw_input('before')
         while Pfvec[max1]*Pfvec[max2]>0:# and max2<len(Pfvec):
             #max2 = max2+int(nd/200)
@@ -458,7 +469,9 @@ def volume_renorm(phase, xint, Pint, bmix, R, T, r_data):
         #min1 = min2+int(nd/100)
         min1 = min2+4
 
+        #print 'int',Pint,xint,phase
         #print 'falsi_spline',rho[max1],rho[max2],rho[min1],rho[min2]
+        #print 'falsi_pressures',Pfvec[max1],Pfvec[max2],Pfvec[min1],Pfvec[min2]
         #Calculate coexistence densities in interpolated isotherm for given P
         rho_vap = numerical.falsi_spline(rho, Pfvec, rho[max1], rho[max2], 1e-5)
         #print 'rho_vap',rho_vap
@@ -506,10 +519,14 @@ def helm_rep(EoS,R,T,rho,amix,bmix,X,x,nc):
         f_CPA = np.dot(np.dot(one4c,x),f_CPA1)
 
     #Considering ideal gas energy contribution
+    V = 1/rho
     f = {
         2: rho*R*T*np.log(rho/(1-rho*bmix))-rho*amix/bmix*np.log(1+rho*bmix), #SRK+RG
         #2: -rho*R*T*np.log(1-rho*bmix)-rho*amix/bmix*np.log(1+rho*bmix)+rho*R*T*(np.log(rho)-1), #SRK+RG
-        4: rho*R*T*np.log(rho/(1-rho*bmix))-rho*amix/bmix*np.log(1+rho*bmix), #PR+RG
+        #4: rho*R*T*np.log(rho/(1-rho*bmix))-rho*amix/bmix*np.log(1+rho*bmix), #PR+RG
+        #4: -rho*(R*T*np.log(V-bmix)-(amix/(2*np.sqrt(2)*bmix)*(np.log(1-(V+bmix)/np.sqrt(2)/bmix)-np.log(1+(V+bmix)/np.sqrt(2)/bmix)))), #PR+RG
+        4: rho*R*T*np.log(rho/(1-rho*bmix))-rho*amix/bmix/np.sqrt(8)*np.log((1+rho*bmix*(1+np.sqrt(2)))/(1+rho*bmix*(1-np.sqrt(2)))), #PR+RG
+        #4: rho*R*T*np.log(rho)+rho*R*T*np.log(1-amix*(V-bmix)/(R*T*(V**2+2*bmix*V-bmix**2)))+rho*amix/(2*np.sqrt(2)*bmix)*np.log((V+(1-np.sqrt(2)*bmix))/(V+(1+np.sqrt(2)*bmix))), #PR+RG
         #4: -rho*R*T*np.log(1-rho*bmix)-rho*amix/bmix*np.log(1+rho*bmix)+rho*R*T*(np.log(rho)-1), #PR+RG
         #6: rho*R*T*(np.log(rho/(1-rho*bmix))-1)-rho*amix/bmix*np.log(1+rho*bmix)+rho*R*T*f_CPA #CPA+RG
         6: rho*R*T*np.log(rho/(1-rho*bmix))-rho*amix/bmix*np.log(1+rho*bmix)+rho*R*T*f_CPA #CPA+RG
@@ -1070,3 +1087,167 @@ def Estimate_Parameters(EoS,IDs,MR,T,Tfinal,stepT,nd,nx,kij,nc,CR,en_auto,beta_a
     par = 1
     return par
 #====================================================================================== 
+
+#Calculates critical point for given mixture composition-------------------------------
+def crit_mix(estimate_crit,Tci,rhoci,EoS,IDs,MR,T,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,x):
+
+    #Initial estimate of critical point
+    if estimate_crit==True:
+        Tci = Tci
+        Vci = rhoci
+    else:
+        Tcvec = data.Tc(IDs)
+        Tci = 1.5*np.dot(x,Tcvec)
+        b = eos.b_calc(IDs,EoS)
+        bmix = eos.bmix_calc(MR,b,x)
+        Vci = 4*bmix
+    Tc = Tci
+    P = 0.1
+    
+    print 'Initial Estimates:','Tc=',Tci,'Vci=',Vci
+    raw_input('...')
+        
+    #Solve det(Q)=0 for initial condition
+    nt = 1.0 #Total number of moles   
+    delta = np.identity(nc) #Kronecker delta
+    
+    #Calculate thermodynamic properties at initial T
+    nx = 4 #Renorm will calculate only given x
+    nd = 400
+    
+    h = 1e-4
+    x0 = np.array([0.1,0.9])
+    nt = np.sum(x0)
+    n0 = nt*x0
+    
+    tolC = 1e-20
+    tolQ = 1e-20
+    C = tolC+1
+    detQ = tolQ+1
+    Q = np.zeros((nc,nc))
+    n1 = np.zeros((nc))
+    n1 = n0
+    
+    r_dat0 = 1
+    r_dat1 = 1
+    
+    
+    #External V loop - solve triple sum = 0
+    while C>tolC:
+        #Internal T loop - solve det(Q)=0------------------------------------------------------------------------------------
+        while detQ>tolQ:
+            
+            dT = 1e-5*Tc
+            
+            #Q at T
+            Q = np.zeros((nc,nc))
+            if EoS==2 or EoS==4 or EoS==6:
+                r_dat0 = renorm(EoS,IDs,MR,Tc,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)    
+            lnfugcoef0 = eos.lnfugcoef_func(IDs,EoS,MR,P,Tc,x0,kij, 1,Vci,en_auto,beta_auto,CR,SM,0,0,r_dat0)[0] #vapor
+            for j in range(0,nc):
+                n1[j] = n0[j]+h
+                x1 = n1/(nt+h)
+                if EoS==2 or EoS==4 or EoS==6:
+                    r_dat1 = renorm(EoS,IDs,MR,Tc,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
+                lnfugcoef1 = eos.lnfugcoef_func(IDs,EoS,MR,P,Tc,x1,kij, 1,Vci,en_auto,beta_auto,CR,SM,0,0,r_dat1)[0] #vapor
+                print lnfugcoef1,x1
+                for i in range(0,nc):
+                    Q[i][j] = (lnfugcoef1[i]-lnfugcoef0[i])/h
+                n1 = n0
+                x1 = x0
+            detQ = np.linalg.det(Q)
+            print 'lnfug0',lnfugcoef0,x0
+            print 'lnfug1',lnfugcoef1,x1
+            print 'detQ=',detQ
+            print 'Q=',Q
+            raw_input('...')
+            
+            #Q at T+dT
+            Q = np.zeros((nc,nc))
+            if EoS==2 or EoS==4 or EoS==6:
+                r_dat0 = renorm(EoS,IDs,MR,Tc+dT,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
+            lnfugcoef0 = eos.lnfugcoef_func(IDs,EoS,MR,P,Tc+dT,x0,kij, 1,Vci,en_auto,beta_auto,CR,SM,0,0,r_dat0)[0] #vapor
+            for j in range(0,nc):
+                n1[j] = n0[j]+h
+                x1 = n1/(nt+h)
+                if EoS==2 or EoS==4 or EoS==6:
+                    r_dat1 = renorm(EoS,IDs,MR,Tc+dT,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
+                lnfugcoef1 = eos.lnfugcoef_func(IDs,EoS,MR,P,Tc+dT,x1,kij, 1,Vci,en_auto,beta_auto,CR,SM,0,0,r_dat1)[0] #vapor
+                for i in range(0,nc):
+                    Q[i][j] = (lnfugcoef1[i]-lnfugcoef0[i])/h
+                n1 = n0
+            detQ_dT = np.linalg.det(Q)
+            
+            dF = (detQ_dT-detQ)/(dT*Tc)
+            Tc = Tc - detQ/dF
+            print 'Tc=',Tc
+            #End Internal T loop - solve det(Q)=0===============================================================================
+            
+        nt = 1.0
+        nt0 = 1.0
+        n0 = x0*nt0
+        dn = 1.0
+        """
+        n1k = np.zeros((4))
+        nt1k = np.zeros((4))
+        x1k = np.zeros((4))
+        k = -2
+        for i in range(0,4)
+            n1k[i] = (x0+k*1e-3*dn/nt0)*nt0
+            nt1k[i] = np.sum(n1)
+            x1k[i] = n1/nt1
+            k = k+1
+        """
+        
+        dV = 1e-6*Vci
+            
+        #C at V
+        C = 0
+        if EoS==2 or EoS==4 or EoS==6:
+            r_dat0 = renorm(EoS,IDs,MR,Tc,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
+        lnfugcoef0 = eos.lnfugcoef_func(IDs,EoS,MR,P,Tc,x0,kij, 1,Vci,en_auto,beta_auto,CR,SM,0,0,r_dat0)[0] #vapor
+        for j in range(0,nc):
+            eps = 1e-3
+            n1[j] = (x0[j]+eps*dn/nt)*nt
+            nt1 = np.sum(n1)
+            x1 = n1/nt1
+            if EoS==2 or EoS==4 or EoS==6:
+                r_dat1 = renorm(EoS,IDs,MR,Tc,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
+            lnfugcoef1 = eos.lnfugcoef_func(IDs,EoS,MR,P,Tc,x1,kij, 1,Vci,en_auto,beta_auto,CR,SM,0,0,r_dat1)[0] #vapor
+            for i in range(0,nc):
+                Q[i][j] = (lnfugcoef1[i]-lnfugcoef0[i])/eps
+                C = C + Q[i][j]*n1[j]*n1[j]
+            n1 = n0
+        
+        #C at T+dV
+        C_dV = 0
+        if EoS==2 or EoS==4 or EoS==6:
+            r_dat0 = renorm(EoS,IDs,MR,Tc,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
+        lnfugcoef0 = eos.lnfugcoef_func(IDs,EoS,MR,P,Tc,x0,kij, 1,Vci+dV,en_auto,beta_auto,CR,SM,0,0,r_dat0)[0] #vapor
+        for j in range(0,nc):
+            eps = 1e-3
+            n1[j] = (x0[j]+eps*dn/nt)*nt
+            nt1 = np.sum(n1)
+            x1 = n1/nt1
+            if EoS==2 or EoS==4 or EoS==6:
+                r_dat1 = renorm(EoS,IDs,MR,Tc,nd,nx,kij,nc,CR,en_auto,beta_auto,SM,n,False,0,0)
+            lnfugcoef1 = eos.lnfugcoef_func(IDs,EoS,MR,P,Tc,x1,kij, 1,Vci+dV,en_auto,beta_auto,CR,SM,0,0,r_dat1)[0] #vapor
+            for i in range(0,nc):
+                Q[i][j] = (lnfugcoef1[i]-lnfugcoef0[i])/eps
+                C_dV = C_dV + Q[i][j]*n1[j]*n1[j]
+            n1 = n0
+        
+        dF = (C_dV-C)/(dV*Vci)
+        Vci = Vci - Vci/dF
+    
+    #Solve det(Q)=0 to find Tc and rhoc
+        
+    print Tc
+    print Vci
+    rhoc = 1/Vci
+    crit = []
+    crit.append(Tc)
+    #crit.append(Pc)
+    crit.append(rhoc)
+    return crit
+#======================================================================================